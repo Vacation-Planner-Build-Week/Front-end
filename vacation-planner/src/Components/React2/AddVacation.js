8fc7a6138a2dcab391e6fae9aed1feb717813a17
Adam <adammonast@Adams-MBP.attlocal.net>
2020-02-04 12:08:37 -0500

    adding the app, login, and utilities worked on yesterday

---
+++ vacation-planner/src/Components/React2/AddVacation.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { axiosWithAuth } from "../Utilities/AxiosWithAuth";
+
+const AddVacation = (props) => {
+    const [ vacation, setVacation ] = useState({
+        vacation_name: "",
+        vacation_description: ""
+    });
+
+    const handleChanges = e => {
+        let value = e.target.value;
+        setVacation({
+            ...vacation,
+            [e.target.name]: value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(vacation)
+        console.log(props.userid)
+        axiosWithAuth()
+        .post(`/users/${props.userid}/vacations`, vacation)
+        .then(response => {
+            console.log(response)
+            props.history.push("/dashboard/");
+            axiosWithAuth()
+            .get(`users/${props.userid}/vacations`)
+            .then(response => {
+                console.log(response)
+                props.setVacation(response.data)
+            })
+            .catch(error => console.log(error))
+        })
+        .catch(error => {
+            console.log("Data was not returned addVacation.js", error)
+            props.history.push("/dashboard/")
+        })
+        props.setUserid(props.userid)
+
+        setVacation({
+            ...vacation,
+            vacation_name: "",
+            vacation_description: ""
+        });
+    };
+
+    return (
+        <div>
+            <h1>Add Vacation</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="name"
+                    type="text"
+                    value={vacation.vacation_name}
+                    onChange={handleChanges}
+                    required
+                />
+                <input
+                    name="description"
+                    type="text"
+                    value={vacation.vacation_description}
+                    onChange={handleChanges}
+                    required
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddVacation;
