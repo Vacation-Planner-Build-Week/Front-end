7c4d131407a59ebf608be2ab9d5048a2f1723df8
Chance Rutledge <chancerutledge2@gmail.com>
2020-02-04 09:06:01 -0800

    registration component wire frame

--- vacation-planner/src/Components/React1/Register.js
+++ vacation-planner/src/Components/React1/Register.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+import React, {useState} from "react";
 
-export default class Register extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Register</h1>
-            </div>
-        )
+
+const Register = () => {
+
+//set up the initial state
+    const [user, setUser] = useState({
+        login:"",
+        password: "",
+        user_email: ""
+    })
+
+//onChange handler
+    const handleChanges = (e) =>{
+    setUser({[e.target.name]:e.target.value})
     }
+
+// submitForm
+    const submitForm = (e) =>{
+    e.preventDefault();
+    setUser({login:"", password: "", user_email: ""})
+
+    }
+
+    return (
+        <div>
+            <form className = "form" onSubmit ={submitForm}>
+                
+                <label htmlFor = "login">LogIn</label>
+                <input class ="input"
+                id = "login"
+                type = "text"
+                name ="login"
+                onChange = {handleChanges}
+                placeholder = "userName"
+                value = {user.user_name}
+                />
+
+
+                <label htmlFor = "email">Email</label> 
+                <input class ="input"
+                id = "email"
+                type ="email"
+                name = "email"
+                onChange = {handleChanges}
+                placeholder = "email@prodiver.com"
+                value = {user.user_email} 
+                />   
+
+
+                <label htmlFor = "password">Password</label> 
+                <input class ="input"
+                id = "password"
+                type ="password"
+                name = "password"
+                onChange = {handleChanges}
+                placeholder = "password"
+                value = {user.user_password} 
+                />   
+               
+                <button type ="submit">Submit</button>
+               
+            </form>
+
+        </div>
+    )
 }
+
+export default Register;
