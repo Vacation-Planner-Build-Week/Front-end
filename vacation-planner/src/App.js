0563c5c1d8ddca48dc5b0908cd6efc82c86e41b6
AshtonRagan <arkjr1@gmail.com>
2020-02-04 17:31:11 -0500

    Redux is set up

--- vacation-planner/src/App.js
+++ vacation-planner/src/App.js
@@ -1,6 +1,6 @@
 // Dependencies
-import React, { useState, useEffect } from "react";
-import { Route, Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
 import { axiosWithAuth } from "./Components/Utilities/AxiosWithAuth";
 import "./App.css";
 
@@ -10,108 +10,49 @@ import LogIn from "./Components/React1/LogIn";
 import PrivateRoute from "./Components/Utilities/PrivateRoute";
 import AddVacation from "./Components/React2/AddVacation";
 import Dashboard from "./Components/React1/Dashboard";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  const [userid, setUserid] = useState(0);
-  const [userdata, setUserdata] = useState([]);
-  const [vacations, setVacations] = useState([]);
-
-  useEffect(() => {
-    setUserid(localStorage.getItem("userid"));
-    if (localStorage.getItem("token")) {
-      axiosWithAuth()
-        .get(`/users/${userid}/vacations`)
-        .then(response => {
-          console.log("Vacation Data: ", response.data);
-          setVacations(response.data);
-        })
-        .catch(error => console.log(error));
-
-      axiosWithAuth()
-        .get(`/users/${userid}/`)
-        .then(response => {
-          console.log("Users Data: ", response.data);
-          setUserdata(response.data);
-        })
-        .catch(error => console.log(error));
-    }
-  }, [userid]);
+  const dispatch = useDispatch();
+  const logged = useSelector(state => state.isLogged);
 
   const signOut = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userid");
-    setUserid(0);
+    dispatch({ type: "LOGOUT_USER" });
   };
 
-  if (!userdata) return null;
-  else if (userdata) {
-    return (
-      <div className="App">
-        <h1>Vacation Planner</h1>
-        <nav className="nav">
-          <div className="nav-links">
-            {!localStorage.getItem("token") && (
-              <Link to="/login/">Sign In</Link>
-            )}
-            {!localStorage.getItem("token") && (
-              <Link to="/signup/">Sign Up</Link>
-            )}
-            {localStorage.getItem("token") && (
-              <Link to="/dashboard/">Dashboard</Link>
-            )}
-            {localStorage.getItem("token") && (
-              <Link to="/addvacation/">Add Vacation</Link>
-            )}
-            {localStorage.getItem("token") && (
-              <Link to="/login/" onClick={signOut}>
-                Sign Out
-              </Link>
-            )}
-          </div>
-        </nav>
-        <Route
-          exact
-          path="/"
-          render={props => <LogIn {...props} setUserid={setUserid} />}
-        />
-        <Route
-          path="/login/"
-          render={props => <LogIn {...props} setUserid={setUserid} />}
-        />
-        {/* <Route 
-          path="/signup/"
-          render={props => 
-            <Register {...props} />
-          } 
-        /> */}
+  return (
+    <div className="App">
+      <h1>Vacation Planner</h1>
+      <nav className="nav">
+        <div className="nav-links">
+          {!localStorage.getItem("token") && <Link to="/">Sign In</Link>}
+          {!localStorage.getItem("token") && <Link to="/signup/">Sign Up</Link>}
+          {localStorage.getItem("token") && (
+            <Link to="/dashboard/">Dashboard</Link>
+          )}
+          {localStorage.getItem("token") && (
+            <Link to="/addvacation/">Add Vacation</Link>
+          )}
+          {localStorage.getItem("token") && (
+            <Link to="/" onClick={signOut}>
+              Sign Out
+            </Link>
+          )}
+        </div>
+      </nav>
+      <Switch>
+        <Route exact path="/" render={props => <LogIn {...props} />} />
         <Route
           path="/addvacation/"
-          component={props => (
-            <AddVacation
-              {...props}
-              userid={userid}
-              setUserid={setUserid}
-              setVacations={setVacations}
-            />
-          )}
+          component={props => <AddVacation {...props} />}
         />
         <PrivateRoute
           path="/dashboard/"
-          component={props => (
-            <Dashboard
-              {...props}
-              userid={userid}
-              setUserid={setUserid}
-              setVacations={setVacations}
-              vacations={vacations}
-              userdata={userdata}
-              setUserdata={setUserdata}
-            />
-          )}
+          component={props => <Dashboard {...props} />}
         />
-      </div>
-    );
-  }
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- vacation-planner/src/Components/Redux/Reducer.js
+++ vacation-planner/src/Components/Redux/Reducer.js
@@ -1,18 +1,37 @@
 export const InitState = {
+  isLogged: false,
   user: {
-    user_Id: 0,
-    user_Name: ""
+    user_Id: null,
+    user_Name: null
   },
   vacations: [{ vacation_name: "New", vacation_description: "Just basic" }]
 };
 
 export const Reducer = (state = InitState, action) => {
   switch (action.type) {
+    case "LOGOUT_USER":
+      localStorage.removeItem("token");
+      localStorage.removeItem("userid");
+      return state;
+
+    case "LOGIN_USER":
+      console.log("USER_LOGIN", action.payload);
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        isLogged: true,
+        user: {
+          user_Id: action.payload.user_id,
+          user_Name: action.payload.user_name
+        }
+      };
+
     case "ADD_VACATION":
       return {
         ...state,
         vacations: [...state.vacations, action.payload]
       };
+
     default:
       return state;
   }
